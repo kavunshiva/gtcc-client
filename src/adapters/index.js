c7004207ba8d680c02ecdc823387b318fd30c5fa
kavunshiva <kavunshiva@gmail.com>
2017-08-15 22:20:25 -0400

    add physicians, appointments

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -1,4 +1,5 @@
 import GregHouseImg from '../assets/images/GregHouse.jpg'
+import DerekShepherdImg from '../assets/images/DerekShepherd.jpg'
 
 export class PhysiciansAdapter {
   static all(){
@@ -11,6 +12,14 @@ export class PhysiciansAdapter {
           education: "University of Michigan",
           description: "An irascible genius struggling with an opiod addiction, he may just figure out what's wrong with you before it's too late.",
           image: GregHouseImg
+        },
+        {
+          id: 15,
+          name: "Derek Shepherd",
+          specialization: "surgeon",
+          education: "Columbia University",
+          description: "Lost causes and impossible cases are no match for the smooth moves and skilled hands of McDreamy.",
+          image: DerekShepherdImg
         }
       ]), 2000))
   }
@@ -28,6 +37,28 @@ export class AppointmentsAdapter {
         "end" : "2015-04-24T07:30:00-06:00",
         "last_date" : "2015-04-02",
         "symptoms" : "mild depression, dysphoria",
+        "created_at" : "2015-04-21T10:44:51-06:00",
+        "updated_at" : "2015-04-21T10:44:51-06:00"
+      },
+      {
+        "id" : 696,
+        "patient_id" : 16,
+        "physician_id" : 15,
+        "start" : "2015-04-25T14:00:00-06:00",
+        "end" : "2015-04-25T14:45:00-06:00",
+        "last_date" : "2015-03-17",
+        "symptoms" : "PTSD, bipolar disorder",
+        "created_at" : "2015-04-21T10:44:51-06:00",
+        "updated_at" : "2015-04-21T10:44:51-06:00"
+      },
+      {
+        "id" : 697,
+        "patient_id" : 16,
+        "physician_id" : 14,
+        "start" : "2017-08-15T22:07:00-06:00",
+        "end" : "2017-08-15T22:30:00-06:00",
+        "last_date" : "2016-09-22",
+        "symptoms" : "body dysmorphic disorder, anorexia nervosa, major depression",
         "created_at" : "2015-04-21T10:44:51-06:00",
         "updated_at" : "2015-04-21T10:44:51-06:00"
       }
